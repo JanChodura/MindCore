26c6ba2a9e009c0ac85e5d251f1ca12edb882a29
Chodura <janzdislavchodura@gmail.cz>
2025-12-02 19:38:37 +0100

    #: repaired to new interface

--- libCore/src/test/kotlin/com/mindjourney/core/observer/trigger/util/SimpleReactiveTestTrigger.kt
+++ libCore/src/test/kotlin/com/mindjourney/core/observer/trigger/util/SimpleReactiveTestTrigger.kt
@@ -1,6 +1,7 @@
 package com.mindjourney.core.observer.trigger.util
 
 import com.mindjourney.core.observer.trigger.model.ReactiveTrigger
+import com.mindjourney.core.observer.trigger.model.TriggerDescription
 import com.mindjourney.core.observer.trigger.model.TriggerResult
 import kotlinx.coroutines.flow.Flow
 
@@ -8,6 +9,8 @@ class SimpleReactiveTestTrigger<T>(
     override val sourceFlow: Flow<T>,
     private val result: TriggerResult,
 ) : ReactiveTrigger<T>(sourceFlow) {
+
+    override lateinit var description: TriggerDescription
 
     override suspend fun tryExecute(): TriggerResult = result
 }
