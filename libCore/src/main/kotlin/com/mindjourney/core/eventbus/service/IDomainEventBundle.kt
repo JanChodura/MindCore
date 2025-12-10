79169b4c51947cbfd788ce2ed797ef5db6f82f1b
Chodura <janzdislavchodura@gmail.cz>
2025-12-10 15:36:35 +0100

    #: vm scope to manager

--- libCore/src/main/kotlin/com/mindjourney/core/eventbus/service/IDomainEventBundle.kt
+++ libCore/src/main/kotlin/com/mindjourney/core/eventbus/service/IDomainEventBundle.kt
@@ -2,8 +2,6 @@ package com.mindjourney.core.eventbus.service
 
 import com.mindjourney.core.eventbus.model.event.context.ObserverContext
 import com.mindjourney.core.eventbus.model.trigger.context.TriggerContext
-import com.mindjourney.core.eventbus.observer.initializer.IDomainObserverInitializer
-import com.mindjourney.core.eventbus.trigger.initializer.ITriggerInitializer
 
 interface IDomainEventBundle {
     val observerContexts: List<ObserverContext>
